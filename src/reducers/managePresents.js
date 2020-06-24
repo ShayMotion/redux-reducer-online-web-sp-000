92eb6976697b9be84ddcc4c3bd284de34a7c256c
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-06-24 15:29:16 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,6 @@
 export function managePresents(state, action){
   let numberOfPresents = {numberOfPresents: 0};
-    switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1};
+   if (action.type === 'INCREASE_COUNT') {
+    return {count: state.count + 1 };
   }
 }
